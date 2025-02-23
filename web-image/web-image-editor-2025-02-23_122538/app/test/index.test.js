ab8e0c5f32b10beaf5444eb42b47c50827c0db03
tejas36026 <73342855+tejas36026@users.noreply.github.com>
2025-02-23 18:26:10 +0530

    Delete web-image directory

--- web-image/web-image-editor-2025-02-23_122538/app/test/index.test.js
+++
@@ -1,7 +0,0 @@
-const { applyEffect, getEffectValue, effects } = require('../src/index');
-
-test('effects array contains expected effects', () => {
-  expect(effects).toContain('brightness');
-  expect(effects).toContain('hue');
-  // Add more effect checks
-});
